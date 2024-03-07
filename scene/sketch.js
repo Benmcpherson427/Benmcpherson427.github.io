d4ca23eda16d1168f41d3ed89d1377cee91e3168
Benmcpherson427 <161518366+Benmcpherson427@users.noreply.github.com>
2024-03-07 14:11:28 -0600

    Update sketch.js

--- scene/sketch.js
+++ scene/sketch.js
@@ -12,30 +12,38 @@ let d;
 let dx;
 let dy;
 let hit;
+let win = false;
+let state = " ";
 
 function setup() {
   x = 390;
   y = 385;
   dx = 3;
   dy = 3;
-  d = 15;
+  d = 10;
   createCanvas(400, 400);
   noStroke();
 }
 
 function draw() {
-  background(255);
-  boundaries();
-  createPath();
-  circel();
-  moveCircle();
-  collisionDetection();
+  if (state === "start"){
+    displayStart();
+  }
+  else if (state === "maze"){
+    background("lightblue");
+    boundaries();
+    createPath();
+    circel();
+    moveCircle();
+    collisionDetection();
+    youWin();
+  }
 
 }
 
 function circel(){
-  fill("cyan");
- circle(x, y, d);
+  fill("orange");
+  circle(x, y, d);
 }
 
 function moveCircle(){
@@ -54,7 +62,7 @@ function moveCircle(){
 }
 
 function boundaries(){
-  if (((x - d/2 <= 0) || (x - d/2 >= 400) || (x + d/2 <= 0) || (x + d/2 >= 400)) || ((y - d/2 <= 0) || (y - d/2 >= 400) || (y + d/2 <= 0) || (y + d/2 >= 400))) {
+  if ((x - d/2 <= 0) || (x - d/2 >= 400) || (x + d/2 <= 0) || (x + d/2 >= 400) || (y - d/2 <= 0) || (y - d/2 >= 400) || (y + d/2 <= 0) || (y + d/2 >= 400)) {
     x = 390;
     y = 385;
   }
@@ -87,19 +95,53 @@ function createPath(){
 } 
 
 function collisionDetection(){
-  hit = (collideRectCircle(200, 370, 200- d, 30- d, x, y, d/2) || collideRectCircle(200, 200, 30- d, 200- d, x, y, d/2) 
-  || collideRectCircle(75, 200, 125- d, 30- d, x, y, d/2) || collideRectCircle(75, 200, 30- d, 120- d, x, y, d/2) 
-  || collideRectCircle(75, 290, 90- d, 30- d, x, y, d/2) || collideRectCircle(0, 150, 30- d, 220- d, x, y, d/2)
-  || collideRectCircle(0, 150, 275- d, 30- d, x, y, d/2) || collideRectCircle(75, 200, 125- d, 30- d, x, y, d/2)
-  || collideRectCircle(135, 290, 30- d, 100- d, x, y, d/2) || collideRectCircle(0, 360, 140- d, 30- d, x, y, d/2)
-  || collideRectCircle(245, 150, 30- d, 200- d, x, y, d/2) || collideRectCircle(245, 320, 150- d, 30- d, x, y, d/2)
-  || collideRectCircle(365, 100, 30- d, 220- d, x, y, d/2) || collideRectCircle(25, 100, 340- d, 30- d, x, y, d/2)
-  || collideRectCircle(25, 0, 30- d, 100- d, x, y, d/2) || collideRectCircle(25, 0, 150- d, 30- d, x, y, d/2)
-  || collideRectCircle(175, 0, 30- d, 70- d, x, y, d/2) || collideRectCircle(175, 40, 130- d, 30- d, x, y, d/2)
-  || collideRectCircle(305, 0, 30- d, 70- d, x, y, d/2) || collideRectCircle(305, 0, 95- d, 30- d, x, y, d/2)
-  || collideRectCircle(370, 0, 30- d, 30- d, x, y, d/2) || collideRectCircle(370, 30, 30- d, 30- d, x, y, d/2));
+  hit = (collideRectCircle(200, 370, 200, 30, x, y, d/2) || collideRectCircle(200, 200, 30, 200, x, y, d/2) 
+  || collideRectCircle(75, 200, 125, 30, x, y, d) || collideRectCircle(200, 370, 200, 30, x, y, d/2) 
+  || collideRectCircle(200, 200, 30, 200, x, y, d/2) || collideRectCircle(75, 200, 125, 30, x, y, d/2)
+  || collideRectCircle(75, 200, 30, 120, x, y, d/2) || collideRectCircle(75, 290, 90, 30, x, y, d/2) 
+  || collideRectCircle(0, 150, 30, 220, x, y, d/2) || collideRectCircle(0, 150, 275, 30, x, y, d/2) 
+  || collideRectCircle(75, 200, 125, 30, x, y, d/2) || collideRectCircle(135, 290, 30, 100, x, y, d/2)
+  || collideRectCircle(0, 360, 140, 30, x, y, d/2) || collideRectCircle(245, 150, 30, 200, x, y, d/2) 
+  || collideRectCircle(245, 320, 150, 30, x, y, d/2) || collideRectCircle(365, 100, 30, 220, x, y, d/2) 
+  || collideRectCircle(25, 100, 340, 30, x, y, d/2) || collideRectCircle(25, 0, 30, 100, x, y, d/2) 
+  || collideRectCircle(25, 0, 150, 30, x, y, d/2) || collideRectCircle(175, 0, 30, 70, x, y, d/2) 
+  || collideRectCircle(175, 40, 130, 30, x, y, d/2) || collideRectCircle(305, 0, 30, 70, x, y, d/2) 
+  || collideRectCircle(305, 0, 95, 30, x, y, d/2) || collideRectCircle(370, 0, 30, 30, x, y, d/2) 
+  || collideRectCircle(370, 30, 30, 30, x, y, d/2));
+
   if (hit === false){
-    x = 390;
-    y = 385;
+    background(255);
+    fill(0);
+    textSize(45);
+    textAlign(CENTER, CENTER);
+    text("You lose! Press mouse to start", width/2, height/2);
+    state = "loser";
+  }
+}
+
+function youWin(){
+  win = collideRectCircle(370, 30, 30, 30, x, y, d/2);
+  if (win === true){
+    background(255);
+    fill(255);
+    textSize(45);
+    textAlign(CENTER, CENTER);
+    text("You Win! Press mouse to start", width/2, height/2);
+    state = "winner";
+  }
+}
+
+function displayStart(){
+  background(255);
+  fill(0);
+  textSize(45);
+  textAlign(CENTER, CENTER);
+  text("Press mouse to start", width/2, height/2);
+
+}
+
+function mousePressed(){
+  if(state === ("start" || "winner" || "loser")){
+    state = "maze";
   }
 }
