adc572208828c8bdea67b7cbcaadbbf855a9d3cf
Benmcpherson427 <161518366+Benmcpherson427@users.noreply.github.com>
2024-04-18 14:10:49 -0600

    jailbreAK

--- jailbreak/sketch.js
+++ jailbreak/sketch.js
@@ -1,15 +1,143 @@
-// Project Title
-// Your Name
-// Date
+// JailBreak
+// Ben McPherson
+// April 17, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let grid;
+let cellSize;
+const GRID_SIZE = 10;
+const PLAYER = 9;
+const BOT = 5;
+const OPENTILE = 0;
+const IMPASSIBLE = 1;
+let player = {
+  x: 0,
+  y: 0,
+}; 
+let state = "start screen";
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+
+  //if randomizing the grid, do this:
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
+  cellSize = height/grid.length;
+
+  //add player to grid
+  grid[player.y][player.x] = PLAYER;
+}
+
+function windowResized() {
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+
+  cellSize = height/grid.length;
 }
 
 function draw() {
-  background(220);
+  if (state === "start screen") {
+    background(0);
+  }
+  else if (state === "game") {
+    background(220);
+    displayGrid();
+  }
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "w") {
+    movePlayer(player.x + 0, player.y - 1);
+  }
+  if (key === "s") {
+    movePlayer(player.x + 0, player.y + 1);
+  }
+  if (key === "a"){
+    movePlayer(player.x - 1, player.y + 0);
+  }
+  if (key === "d") {
+    movePlayer(player.x + 1, player.y + 0);
+  }
+
+  if (key === " " && state === "start screen"){
+    state = "game";
+  }
+}
+
+function movePlayer(x, y) {
+  if (x < GRID_SIZE && y < GRID_SIZE && x >= 0 && y >= 0 && grid[y][x] === OPENTILE) {
+    let oldx = player.x;
+    let oldy = player.y;
+
+    player.x = x;
+    player.y = y;
+
+    grid[oldy][oldx] = OPENTILE;
+
+    grid[player.y][player.x] = PLAYER;
+  }
+}
+
+
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === OPENTILE) {  
+        fill("white");
+      }
+      else if (grid[y][x] === PLAYER)  {
+        fill("red");
+      }
+      else if (grid[y][x] === BOT) {
+        fill("blue");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
 }
