83fcd36d83b2bf5607012ac70ddbae8772970cd0
Benmcpherson427 <161518366+Benmcpherson427@users.noreply.github.com>
2024-04-29 14:12:14 -0600

    RNGmaze

--- RNGmaze/sketch.js
+++ RNGmaze/sketch.js
@@ -11,10 +11,10 @@ let grid = [[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1
             [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
             [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
             [0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
-            [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0],
-            [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1],
             [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1],
-            [0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+            [0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
             [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
             [1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
@@ -22,26 +22,32 @@ let grid = [[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1
             [1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
             [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
             [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-            [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
             [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 5],
             [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
             [0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-            [0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-            [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+            [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 let cellSize;
 const GRID_SIZE = 25;
 const PLAYER = 9;
 const OPENTILE = 0;
 const IMPASSIBLE = 1;
-const WIN = 5;
+const END = 5;
+const WIN = 7;
 let player = {
   x: 0,
   y: 0,
 }; 
 let state = "start screen";
+let ends = [grid[22][2], 
+            grid[5][23], 
+            grid[8][13], 
+            grid[18][24], 
+            grid[16][24]];
 
 function setup() {
   //make the canvas the largest square that you can...
@@ -79,6 +85,15 @@ function draw() {
   else if (state === "game") {
     background(220);
     displayGrid();
+
+    let theEnd = {x: 4, y: 2};
+    fill('green');
+    square(theEnd.x * cellSize, theEnd.y * cellSize, cellSize);
+
+  }
+  else if (state === "win") {
+    background(255);
+    text
   }
 }
 
@@ -89,21 +104,25 @@ function keyPressed() {
   if (key === "w") {
     movePlayer(player.x + 0, player.y - 1);
     randomReset();
+    endJump();
   
   }
   if (key === "s") {
     movePlayer(player.x + 0, player.y + 1);
     randomReset();
+    endJump();
   
   }
   if (key === "a"){
     movePlayer(player.x - 1, player.y + 0);
     randomReset();
+    endJump();
   
   }
   if (key === "d") {
     movePlayer(player.x + 1, player.y + 0);
     randomReset();
+    endJump();
   }
 
   if (key === " " && state === "start screen"){
@@ -123,6 +142,15 @@ function movePlayer(x, y) {
 
     grid[player.y][player.x] = PLAYER;
   }
+  else if (x <= GRID_SIZE && y <= GRID_SIZE && x >= 0 && y >= 0 && grid[y][x] === END) {
+    let oldx = player.x;
+    let oldy = player.y;
+
+    player.x = x;
+    player.y = y;
+
+    grid[oldy][oldx] = WIN;
+  }
 }
 
 
@@ -131,6 +159,9 @@ function displayGrid() {
     for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] === OPENTILE) {  
         fill("white");
+      }
+      else if (grid[y][x] === END) {
+        fill("green");
       }
       else if (grid[y][x] === PLAYER)  {
         fill("red");
@@ -143,8 +174,21 @@ function displayGrid() {
   }
 }
 
+function endJump() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (random(5) < 1) {
+        let winTile = random(ends);
+        winTile = END;
+        fill("green");
+        square(x * cellSize, y * cellSize, cellSize);
+      }
+    }
+  }
+}
+
 function randomReset() {
-  if (random(30) < 1) {
+  if (random(25) < 1) {
     let oldx = player.x;
     let oldy = player.y;
 
@@ -154,6 +198,11 @@ function randomReset() {
     player.y = 0;
     grid[0][0] = PLAYER;
 
+  }
+}
+function winner() {
+  if (grid[y][x] === WIN) {
+    state = "win";
   }
 }
 
